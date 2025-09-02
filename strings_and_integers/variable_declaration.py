1b74120798ba1bb1a91c9245f9ef38af59655e38
antonyreis <antony.matheus@eyeflow.ai>
2025-09-02 14:37:58 -0300

    add: strings_and_integers station created

---
+++ strings_and_integers/variable_declaration.py
@@ -0,0 +1,12 @@
+"""
+1. In this very first mission you just need to help our bunch of cyborgs to make friends! Look at the example in the editor, declare two variables robots, droids and set their values to integers 2, 5 respectively. If you have any trouble, look at the hints below the description.
+
+2. When done, try to set values for variables from the previous paragraph in a single line like var1, var2 = value1, value2 (first variable gets first value etc.).
+
+While Python remains a dynamically typed language, meaning you don't have to specify types for variables, incorporating type annotations, especially in larger projects or when working in a team, can significantly enhance code quality and maintainability. It's worth noting that type annotations are optional, and Python will not enforce strict typing; they serve primarily as a development aid and documentation tool.
+
+3. For integers use int type. Try to rewrite the task from the first paragraph with type annotations.
+
+"""
+
+a, b = int(2), 5
