09132189d2d057a4d437af7a366ba8017ca4082d
antonyreis <antony.matheus@eyeflow.ai>
2025-09-03 18:06:18 -0300

    add: new solution added

--- python/look_up_in_the_dict/yaml_more_types.py
+++ python/look_up_in_the_dict/yaml_more_types.py
@@ -1,20 +1,50 @@
 # Taken from mission YAML. Simple Dict
 
 def yaml(a: str) -> dict:
+
     text_dict = a.splitlines()
     final = {}
+
     for text in text_dict:
         _list = text.split(":")
+
         if len(_list) < 2:
+            print(_list[0], _list[-1])
             continue
         
-        final[_list[0]] = int(_list[-1].strip()) if _list[-1].strip().isdigit() else _list[-1].strip()
+        if _list[-1].strip().isdigit():
+            final[_list[0]] = int(_list[-1].strip())
+            continue
+
+        elif _list[-1].strip().startswith('"') and _list[-1].strip().endswith('"'):
+
+            if '\\"' in _list[-1].strip().strip('"'):
+                final[_list[0]] = _list[-1].strip().replace('\\', '')[1:-1]
+
+            else:
+                final[_list[0]] = _list[-1].strip().strip('"')
+
+            continue
+
+        elif _list[-1].strip().lower() == 'false':
+            final[_list[0]] = False
+            continue
+
+        elif _list[-1].strip().lower() == 'true':
+            final[_list[0]] = True
+            continue
+
+        elif _list[-1].strip().lower() in ('null', 'none') or _list[-1].strip() == '':
+            final[_list[0]] = None
+            continue
+        
+        final[_list[0]] = _list[-1].strip()
+
     return final
 
 
 print("Example:")
-print(yaml("name: Alex\nage: 12"))
-print(yaml("name: Alex Fox\nage: 12\n\nclass: 12b"))
+print(yaml('name: "Bob Dylan"\nchildren: 6\ncoding:'))
 
 # These "asserts" are used for self-checking
 assert yaml("name: Alex\nage: 12") == {"name": "Alex", "age": 12}
